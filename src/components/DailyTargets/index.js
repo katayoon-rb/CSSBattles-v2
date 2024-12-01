d1e861303b0a576b0251d4618141a4ab9c06cd21
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-12-01 13:05:19 +0330

    Update index.js

--- src/components/DailyTargets/index.js
+++ src/components/DailyTargets/index.js
@@ -4,8 +4,8 @@ function DailyTargets() {
     const months = ['October 2023', 'November 2023', 'December 2023',
                     'January 2024', 'February 2024', 'March 2024', 'April 2024',
                     'May 2024', 'June 2024', 'July 2024', 'August 2024', 'September 2024',
-                   'October 2024', 'November 2024']
-    const days = ['24', '54', '84', '115', '146', '175', '206', '236', '267', '297', '328', '359', '389', '420']
+                   'October 2024', 'November 2024', 'December 2024']
+    const days = ['24', '54', '84', '115', '146', '175', '206', '236', '267', '297', '328', '359', '389', '420', '450']
     months.reverse()
     days.reverse()
 
